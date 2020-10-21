2478659136ea33dba948cbfa34eab5a0c3f7764d
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-21 21:31:31 +0700

    addswitch

--- main.js
+++ main.js
@@ -22,3 +22,27 @@ function doingay(){
      document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "không tồn tại";
        
     }
+function doingaysiwtch(){
+    let thang = Number(document.getElementById("thang").value);
+    switch (thang){
+        case 2:
+        document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 28 ngày hoặc 29 ngày";
+        break
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 31 ngày";
+        break
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 30 ngày"
+        default:
+            document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "không tồn tại"
+    }
+}
