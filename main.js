6fb58cf01eee5dcb44bb583afac409e4a7e39883
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-21 20:52:20 +0700

    endif

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 function doingay(){
     let thang = Number(document.getElementById("thang").value);
-    alert(thang);
-    if (thang == 1){
-
+   
+    if (thang == 1 
+        || thang == 3
+        || thang == 5
+        || thang == 7
+        || thang == 8
+        || thang == 10
+        || thang == 12 )
+        {
+        document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 31 ngày";
+     } else if (thang == 2){
+        document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 30 ngày";
+     } else if (thang == 4 
+        || thang == 6
+        || thang == 9
+        || thang == 11
+         ){
+       
+     document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "có số ngày là 28 ngày hoặc 29 ngày";}
+     document.getElementById("hienthingay").innerHTML = "Tháng" + " " + thang + " " + "không tồn tại";
+       
     }
-}
